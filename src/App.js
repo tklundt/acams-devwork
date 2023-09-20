4e60a8c064976d3541809ab3894b97a3e1178001
Trey Klundt <tklundt@asu.edu>
2023-09-19 19:12:14 -0700

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>ACAMS Asset System</p>
-      </header>
-      <img src="ACAMS_Logo.png"></img>
-    </div>
+    <div class="navbar">
+      <div class="nav-logo"><img src="ACAMS_Logo.png" width="5%" height="5%"></img></div>
+      <div class="nav-list">
+          <ul>
+          <li class="nav-item"><a href="">Login</a></li>
+          <li class="nav-item"><a href="">Assets</a></li>
+          <li class="nav-item"><a href="">Admin</a></li>
+          <li class="nav-item"><a href="">Ticket</a></li>
+        </ul></div>
+      </div>
   );
 }
 
